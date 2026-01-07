c1d4a555ead01ab86f757b7eb782555cac9ecb49
duncanokeyo <okeyodan@gmail.com>
2026-01-07 13:21:11 +0300

    fixes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/online/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: { '@': resolve(__dirname, 'src') }
